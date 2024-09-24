443ba3bec2734dc05ab83c6160b4a8cb8545dbcc
Ale Lima <Ale>
2024-09-24 19:23:55 -0300

    Projeto Vetores

---
+++ src/App.java
@@ -0,0 +1,22 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        String[][] alunos = {
+            {"ALE","10"},
+            {"ANA","8"},
+            {"MARIA","4"},
+            {"JOÃO","7"},
+        };
+
+        alunos[3][0] = "PEDRO";
+        
+
+        for(String[] linhas:alunos){
+            for(String coluna:linhas){
+                System.out.print(coluna+ " ");
+            }
+            System.out.println();
+        }
+        
+        
+    }
+}
